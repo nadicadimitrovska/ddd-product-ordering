5062890c7fdaccb0d435bb689b3abc6bb3d4cb88
Nadica Dimitrovska <you@example.com>
2023-05-21 23:42:08 +0200

    Auditory Exercise 10

---
+++ src/test/java/mk/ukim/finki/emt/sharedkernel/SharedKernel1ApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.emt.sharedkernel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharedKernel1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
