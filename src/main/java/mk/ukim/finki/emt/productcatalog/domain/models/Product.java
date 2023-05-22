e73ea97b3210d32872b9a1ac9cb732fa6354c69d
Nadica Dimitrovska <you@example.com>
2023-05-22 17:18:10 +0200

    Auditory Exercise 11

--- src/main/java/mk/ukim/finki/emt/productcatalog/domain/models/Product.java
+++ src/main/java/mk/ukim/finki/emt/productcatalog/domain/models/Product.java
@@ -12,11 +12,31 @@ public class Product extends AbstractEntity<ProductId> {
 
     private String productName;
 
-    private Quantity quantity;
+    private int sales = 0;
 
     @AttributeOverrides({
             @AttributeOverride(name = "amount", column = @Column(name = "price_amount")), @AttributeOverride(name = "currency", column = @Column(name = "price_currency"))
     })
     private Money price;
+
+    private Product() {
+        super(ProductId.randomId(ProductId.class));
+    }
+
+    public static Product build(String productName, Money price, int sales) {
+        Product p = new Product();
+        p.price = price;
+        p.productName = productName;
+        p.sales = sales;
+        return p;
+    }
+
+    public void addSales(int qty) {
+        this.sales = this.sales - qty;
+    }
+
+    public void removeSales(int qty) {
+        this.sales -= qty;
+    }
 
 }
